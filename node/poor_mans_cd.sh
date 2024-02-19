2b232a8e2f2735e2c45bc02b800750cce6e18a8f
Ahmed Bayoumy <ahbayo@outlook.com>
2024-02-19 00:28:44 -0600

    fix poor_man_cd race conditions (#52)

--- node/poor_mans_cd.sh
+++ node/poor_mans_cd.sh
@@ -2,19 +2,39 @@
 
 start_process() {
     GOEXPERIMENT=arenas go run ./... &
-    process_pid=$!
-    child_process_pid=$(pgrep -P $process_pid)
+    local process_pid=$!
+    local child_process_pid=$(pgrep -P $process_pid)
+    echo "Process started with PID $process_pid and child PID $child_process_pid"
 }
 
 is_process_running() {
+    local process_pid=$(ps -ef | grep "exe/node" | grep -v grep | awk '{print $2}')
     ps -p $process_pid > /dev/null 2>&1
     return $?
 }
 
 kill_process() {
-    kill $process_pid
-    kill $child_process_pid
+    local process_count=$(ps -ef | grep "exe/node" | grep -v grep | wc -l)
+    local process_pids=$(ps -ef | grep "exe/node" | grep -v grep | awk '{print $2}' | xargs)
+
+    if [ $process_count -gt 0 ]; then
+        echo "killing processes $process_pids"
+        kill $process_pids
+
+        local child_process_count=$(pgrep -P $process_pids | wc -l)
+        local child_process_pids=$(pgrep -P $process_pids | xargs)
+        if [ $child_process_count -gt 0 ]; then
+            echo "killing child processes $child_process_pids"
+            kill $child_process_pids
+        else
+            echo "no child processes running"
+        fi
+    else
+        echo "no processes running"
+    fi
 }
+
+kill_process
 
 start_process
 
