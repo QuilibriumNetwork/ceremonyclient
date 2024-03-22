2c12b35be15167cd9c7928f8b4f0d9fd3ccb77cf
Marius Scurtescu <marius.scurtescu@gmail.com>
2024-03-21 19:49:56 -0700

    return version with GetNodeInfo response

--- node/rpc/rpc_server.go
+++ node/rpc/rpc_server.go
@@ -5,6 +5,7 @@ import (
 	"context"
 	"math/big"
 	"net/http"
+	"source.quilibrium.com/quilibrium/monorepo/node/config"
 
 	"github.com/libp2p/go-libp2p/core/peer"
 
@@ -165,7 +166,7 @@ func (r *RPCServer) GetNodeInfo(
 
 	peerScore := r.pubSub.GetPeerScore(r.pubSub.GetPeerID())
 
-	return &protobufs.NodeInfoResponse{PeerId: peerID.String(), MaxFrame: maxFrame.FrameNumber, PeerScore: uint64(peerScore)}, nil
+	return &protobufs.NodeInfoResponse{PeerId: peerID.String(), MaxFrame: maxFrame.FrameNumber, PeerScore: uint64(peerScore), Version: config.GetVersion()}, nil
 }
 
 // GetPeerInfo implements protobufs.NodeServiceServer.
