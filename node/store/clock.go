eb6e5c97e7eadab2cb1fdb0f81f45549d75183fe
Victor Shyba <shyba@users.noreply.github.com>
2024-03-24 05:11:30 -0300

    skip compaction missing data (#143)

--- node/store/clock.go
+++ node/store/clock.go
@@ -1277,7 +1277,7 @@ func (p *PebbleClockStore) Compact(
 		idxValue, closer, err := p.db.Get(clockDataLatestIndex(dataFilter))
 		if err != nil {
 			if errors.Is(err, pebble.ErrNotFound) {
-				return errors.Wrap(err, "compact")
+				return ErrNotFound
 			}
 
 			return errors.Wrap(err, "compact")
@@ -1291,7 +1291,7 @@ func (p *PebbleClockStore) Compact(
 			value, closer, err := p.db.Get(clockDataFrameKey(dataFilter, frameNumber))
 			if err != nil {
 				if errors.Is(err, pebble.ErrNotFound) {
-					return errors.Wrap(err, "compact")
+					return ErrNotFound
 				}
 
 				return errors.Wrap(err, "compact")
