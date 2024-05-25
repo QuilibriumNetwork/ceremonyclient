cfe77453c685738cf40b559af920ec7265b941da
Cassandra Heart <cassandra@quilibrium.com>
2024-05-25 00:11:06 -0500

    update script

--- node/release_autorun.sh
+++ node/release_autorun.sh
@@ -5,18 +5,20 @@ start_process() {
     if [[ "$OSTYPE" == "linux-gnu"* ]]; then
         if [[ $arch == arm* ]]; then
             ./node-$version-linux-arm64 &
+            main_process_id=$!
         else
             ./node-$version-linux-amd64 &
+            main_process_id=$!
         fi
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         ./node-$version-darwin-arm64 &
+        main_process_id=$!
     else
         echo "unsupported OS for releases, please build from source"
         exit 1
     fi
-    main_process_id=$!
-    local child_process_pid=$(pgrep -P $main_process_id)
-    echo "process started with PID $main_process_id and child PID $child_process_pid"
+
+    echo "process started with PID $main_process_id"
 }
 
 is_process_running() {
@@ -25,21 +27,12 @@ is_process_running() {
 }
 
 kill_process() {
-    local process_count=$(ps -ef | grep "exe/node" | grep -v grep | wc -l)
-    local process_pids=$(ps -ef | grep "exe/node" | grep -v grep | awk '{print $2}' | xargs)
+    local process_count=$(ps -ef | grep "node-$version" | grep -v grep | wc -l)
+    local process_pids=$(ps -ef | grep "node-$version" | grep -v grep | awk '{print $2}' | xargs)
 
     if [ $process_count -gt 0 ]; then
         echo "killing processes $process_pids"
         kill $process_pids
-
-        local child_process_count=$(pgrep -P $process_pids | wc -l)
-        local child_process_pids=$(pgrep -P $process_pids | xargs)
-        if [ $child_process_count -gt 0 ]; then
-            echo "killing child processes $child_process_pids"
-            kill $child_process_pids
-        else
-            echo "no child processes running"
-        fi
     else
         echo "no processes running"
     fi
