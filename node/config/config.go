ac1b98dfda8abc87e20a2331fabee465fefe5c9b
mjessup <matthewjessup44@gmail.com>
2024-07-19 20:15:26 -0400

    Add new Bootstrap Peer  (#276)
    * Add new Bootstrap Peer Update config.go
    
    Added info for bootstrap peer
    
    * Update config.go
    
    needed preceding slash
    
    ---------
    
    Co-authored-by: Cassandra Heart <7929478+CassOnMars@users.noreply.github.com>

--- node/config/config.go
+++ node/config/config.go
@@ -65,6 +65,7 @@ var BootstrapPeers = []string{
 	"/ip4/204.12.220.2/udp/8336/quic-v1/p2p/QmRw5Tw4p5v2vLPvVSAkQEiRPQGnWk9HM4xiSvgxF82CCw",
 	"/ip4/209.159.149.14/udp/8336/quic-v1/p2p/Qmcq4Lmw45tbodvdRWZ8iGgy3rUcR3dikHTj1fBXP8VJqv",
 	"/dns/bravo-1.qcommander.sh/udp/8336/quic-v1/p2p/QmWFK1gVuhEqZdr8phTo3QbyLwjYmyivx31Zubqt7oR4XB",
+	"/ip4/138.201.203.208/udp/8336/quic-v1/p2p/QmbNhSTd4Y64ZCbV2gAXYR4ZFDdfRBMfrgWsNg99JHxsJo",
 	// purged peers (keep your node online to return to this list)
 	// "/ip4/204.186.74.47/udp/8317/quic-v1/p2p/Qmd233pLUDvcDW3ama27usfbG1HxKNh1V9dmWVW1SXp1pd",
 	// "/ip4/186.233.184.181/udp/8336/quic-v1/p2p/QmW6QDvKuYqJYYMP5tMZSp12X3nexywK28tZNgqtqNpEDL",
